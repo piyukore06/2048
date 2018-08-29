5298767a00ccf980f235555649569461f733b7c1
Priyanka Shete <shete@mehrkanal.com>
2018-08-29 22:13:47 +0200

    Moving appropriate methods in separate files

--- src/Components/Container.js
+++ src/Components/Container.js
@@ -4,21 +4,22 @@ import './../Styles/Container.css';
 import FieldRow from './FieldRow';
 import { StatusImage } from './StatusImage';
 import * as Constants from './../Functions/Constants';
+import * as Helpers from './../Functions/Helpers';
 
 class Container extends Component {
   state = null;
   componentWillMount() {
     this.setInitialState();
-    document.addEventListener('keydown', this.handleKeyDown);
   }
   componentWillUnmount() {
     document.removeEventListener('keydown', this.handleKeyDown);
   }
   setInitialState = () => {
     this.setState({
-      tiles :  Array(Constants.GridSize).fill(null).map(_ => this.getRandomRow()),
+      tiles : Array(Constants.GridSize).fill(null).map(_ => Helpers.getRandomRow()),
       status: Constants.Status.START
     });
+    document.addEventListener('keydown', this.handleKeyDown);
   }
   handleKeyDown = (event) => {
     const tiles = this.getAlteredTiles(event.key);
@@ -28,10 +29,7 @@ class Container extends Component {
     });
     this.checkStatus();
   }
-  // 2d Matrix transpose method
-  transpose = (prev, next) => next.map((item, i) => (prev[i] || []).concat(next[i]));
 
-  reverseRow = row => row.reverse();
 
   // checking tiles status to output the status image  
   checkStatus = () => {
@@ -42,104 +40,64 @@ class Container extends Component {
       status
     });
   }
-  getAlteredTiles = (key) => {
-    const randomRowIndex = this.getRandomNumber();
-    const randomNumber = this.getRandomNumber({min: 1, max: 2});
-    switch (key) {
-      case 'ArrowDown':
-        return this.state.tiles
-          .reduce(this.transpose, [])
-          .map(this.reverseRow)
-          .map(this.alterRow)
-          .map((row, index) => this.addRandomField(row, index, randomRowIndex, randomNumber))
-          .map(this.reverseRow)
-          .reduce(this.transpose, [])
-
-      case 'ArrowUp':
-        return this.state.tiles
-            .reduce(this.transpose, [])
-            .map(this.alterRow)
-            .map((row, index) => this.addRandomField(row, index, randomRowIndex, randomNumber))
-            .reduce(this.transpose, []);
-
-      case 'ArrowRight':
-        return this.state.tiles
-            .map(this.reverseRow)
-            .map(this.alterRow)
-            .map((row, index) => this.addRandomField(row, index, randomRowIndex, randomNumber))
-            .map(this.reverseRow)
-
-      case 'ArrowLeft':
-        return this.state.tiles
-          .map(this.alterRow)
-          .map((row, index) => this.addRandomField(row, index, randomRowIndex, randomNumber))
-
-      default:
-        break;
-    }
-  }
   getStatus = (tiles) => {
     const doesProgressMoveTileExist = tiles.some(value => Constants.ProgressMoves.indexOf(value) !== -1);
     const doesWinningTileExist = tiles.filter(value => value === Constants.WinningTile).length;
     const doesAlmostWinningTileExist = tiles.filter(value => value === Constants.AlmostWinningTile).length;
     const doesZeroExist = tiles.filter(value => value === 0).length;
-    if (doesProgressMoveTileExist) {
-      const randomImage = this.getRandomNumber({ min: 0, max: 3 });
-      return randomImage === 3 ? null :Constants.Status.MOVE[randomImage];
-    } else if (!doesZeroExist) {
+    if (!doesZeroExist) {
       document.removeEventListener('keydown', this.handleKeyDown);
       return Constants.Status.LOST;
+    } else if (doesProgressMoveTileExist) {
+      const randomImage = Helpers.getRandomNumber({ min: 0, max: 3 });
+      return randomImage === 3 ? null :Constants.Status.MOVE[randomImage];
     } else if (doesAlmostWinningTileExist) {
       return Constants.Status.ALMOST_WON
     } else if (doesWinningTileExist) {
       return Constants.Status.WON
     }
   }
+  getAlteredTiles = (key) => {
+    const randomRowIndex = Helpers.getRandomNumber({min: 0, max: Constants.GridSize - 1});
+    const randomNumber = Helpers.getRandomNumber({min: 1, max: 2});
+    switch (key) {
+      case 'ArrowDown':
+        return this.state.tiles
+          .reduce(Helpers.transpose, [])
+          .map(Helpers.reverseRow)
+          .map(Helpers.alterRow)
+          .map((row, index) => Helpers.addRandomField(row, index, randomRowIndex, randomNumber))
+          .map(Helpers.reverseRow)
+          .reduce(Helpers.transpose, [])
 
-  // chage row on key press
-  // defined basically for Keypress ArrowLeft
-  // Same Logic is used for other Keypress Events changing the array order in appropriate manner 
-  alterRow = (row) => {
-    let saveRepeatingIndex = [];
-    row.forEach((fieldColumn, columnIndex) => {
-      if (columnIndex < Constants.GridSize - 1 && fieldColumn === row[columnIndex + 1]) {
-        saveRepeatingIndex.push(columnIndex)
-      }
-    });
-    if (saveRepeatingIndex.length === Constants.GridSize - 1) {
-      saveRepeatingIndex = saveRepeatingIndex.filter((repeatingIndex, index) => index % 2 !== 0);
+      case 'ArrowUp':
+        return this.state.tiles
+            .reduce(Helpers.transpose, [])
+            .map(Helpers.alterRow)
+            .map((row, index) => Helpers.addRandomField(row, index, randomRowIndex, randomNumber))
+            .reduce(Helpers.transpose, []);
+
+      case 'ArrowRight':
+        return this.state.tiles
+            .map(Helpers.reverseRow)
+            .map(Helpers.alterRow)
+            .map((row, index) => Helpers.addRandomField(row, index, randomRowIndex, randomNumber))
+            .map(Helpers.reverseRow)
+
+      case 'ArrowLeft':
+        return this.state.tiles
+          .map(Helpers.alterRow)
+          .map((row, index) => Helpers.addRandomField(row, index, randomRowIndex, randomNumber))
+
+      default:
+        break;
     }
-    saveRepeatingIndex.forEach(_ => {
-      for (let index = 0; index <= row.length; index++) {
-        if (index < Constants.GridSize - 1 && row[index] === row[index + 1]) {
-          row[index] = row[index] * 2;
-          row = [...row.slice(0, index + 1), ...row.slice(index + 2)];
-          break;
-        }
-      }
-    });
-    row = row.filter(field => field !== 0);
-    return [...row, ...Array(Constants.GridSize - row.length).fill(0)];
+    return this.state.tiles;
   }
-
-  // Random Field is added taking into consideration the default AlterRow Keypress ArrowLeft
-  // This is to be called immidieately after alterRow to have field creation at correct place
-  addRandomField = (row, index, randomRowIndex, randomNumber) => {
-    if (index === randomRowIndex) {
-      row[Constants.GridSize - 1] = randomNumber;
-    }
-    return row;
-  }
-
-  getRandomNumber = ({ min, max } = { min: 0, max: 2 }) => {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-  getRandomRow = () => {
-    return Array(Constants.GridSize).fill(null).map(_ => this.getRandomNumber());
-  }
-
+  
   render() {
     const { tiles, status } = this.state;
+    console.log(tiles);
     return (
       <Fragment>
         <div className="Container">
---
+++ src/Functions/Helpers.js
@@ -0,0 +1,49 @@
+import * as Constants from './Constants';
+
+// chage row on key press
+// defined basically for Keypress ArrowLeft
+// Same Logic is used for other Keypress Events changing the array order in appropriate manner 
+export const alterRow = (row) => {
+  let saveRepeatingIndex = [];
+  row.forEach((fieldColumn, columnIndex) => {
+    if (columnIndex < Constants.GridSize - 1 && fieldColumn === row[columnIndex + 1]) {
+      saveRepeatingIndex.push(columnIndex)
+    }
+  });
+  if (saveRepeatingIndex.length === Constants.GridSize - 1) {
+    saveRepeatingIndex = saveRepeatingIndex.filter((repeatingIndex, index) => index % 2 === 0);
+  }
+  console.log(saveRepeatingIndex);
+  saveRepeatingIndex.reverse().forEach(_ => {
+    for (let index = 0; index <= row.length; index++) {
+      if (index < Constants.GridSize - 1 && row[index] === row[index + 1]) {
+        row[index] = row[index] * 2;
+        row = [...row.slice(0, index + 1), ...row.slice(index + 2)];
+        break;
+      }
+    }
+  });
+  row = row.filter(field => field !== 0);
+  return [...row, ...Array(Constants.GridSize - row.length).fill(0)];
+}
+
+// Random Field is added taking into consideration the default AlterRow Keypress ArrowLeft
+// This is to be called immidieately after alterRow to have field creation at correct place
+export const addRandomField = (row, index, randomRowIndex, randomNumber) => {
+  if (index === randomRowIndex) {
+    row[Constants.GridSize - 1] = randomNumber;
+  }
+  return row;
+}
+
+export const getRandomNumber = ({ min, max } = { min: 0, max: 2 }) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+export const getRandomRow = () => {
+  return Array(Constants.GridSize).fill(null).map(_ => getRandomNumber());
+}
+
+  // 2d Matrix transpose method
+export const transpose = (prev, next) => next.map((item, i) => (prev[i] || []).concat(next[i]));
+
+export const reverseRow = row => row.reverse();
